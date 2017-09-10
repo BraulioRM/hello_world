900a610b2a8539510651cd612de0d28a16f9adfc
Braulio <ramirez.braulioa@gmail.com>
2017-09-10 11:00:59 -0500

    config.application.assets in the top

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative 'boot'
 
 require 'rails/all'
 
+config.assets.initialize_on_precompile = false
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -13,7 +15,7 @@ module HelloApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.assets.initialize_on_precompile = false
-    config.assets.enabled = true
+    
     
 
     # Settings in config/environments/* take precedence over those specified here.
